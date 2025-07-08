a9aba109b4f635d12dc97a0475fd5356439d835a
Runxi Shen <shenrunxi@gmail.com>
2025-07-08 14:09:12 +0000

    Updating all three assay analyses

--- 2_individual_assay_analyses/imaging/1_inputs/1_snakemake_pipeline/download_aws_cpg_data.sh
+++ 2_individual_assay_analyses/imaging/1_inputs/1_snakemake_pipeline/download_aws_cpg_data.sh
@@ -5,11 +5,13 @@
 ## aws cpg paths, publicly available
 AWS_IMG_PATH="s3://cellpainting-gallery/cpg0020-varchamp/broad/images"
 AWS_WORKSPACE_PATH="s3://cellpainting-gallery/cpg0020-varchamp/broad/workspace"
+AWS_ANALYSIS_PATH="s3://cellpainting-gallery/cpg0020-varchamp/broad/workspace/analysis"
 
 ## local paths for store the aws cpg data
-CPG_IMG_PATH="./1.image_preprocess_qc/inputs/cpg_imgs" ## symbolic link to /data/shenrunx/igvf/varchamp/2021_09_01_VarChAMP_imgs
-SNAKEMAKE_INPUT_PATH="./2.snakemake_pipeline/inputs"
-BATCHES="2024_01_23_Batch_7 2024_02_06_Batch_8 2024_12_09_Batch_11 2024_12_09_Batch_12 2025_03_17_Batch_15 2025_03_17_Batch_16"
+CPG_IMG_PATH="cpg_imgs" ## symbolic link to /data/shenrunx/igvf/varchamp/2021_09_01_VarChAMP_imgs
+SNAKEMAKE_INPUT_PATH="inputs"
+# BATCHES="2025_01_28_Batch_14 2025_01_27_Batch_13"
+BATCHES="2025_06_10_Batch_18 2025_06_10_Batch_19"
 
 ## create directories
 # mkdir -p $CPG_IMG_PATH
@@ -20,18 +22,18 @@ for batch_id in $BATCHES;
 do
     ## download the raw img data
     # aws s3 sync --no-sign-request "$AWS_IMG_PATH/$batch_id/images" $CPG_IMG_PATH/$batch_id/images
-    # aws s3 sync --no-sign-request \
-    #     "$AWS_ANALYSIS_PATH/$batch_id" \
-    #     "$CPG_IMG_PATH/$batch_id/analysis" \
-    #     --exclude "*" \
-    #     --include "**/Cells.csv" \
-    #     --include "**/Cytoplasm.csv" \
-    #     --include "**/Nuclei.csv" \
-    #     --include "**/Image.csv" \
-        # --recursive \
+    aws s3 sync --no-sign-request \
+        "$AWS_ANALYSIS_PATH/$batch_id" \
+        "$CPG_IMG_PATH/$batch_id/analysis" \
+        --exclude "*" \
+        --include "**/Cells.csv" \
+        --include "**/Cytoplasm.csv" \
+        --include "**/Nuclei.csv" \
+        --include "**/Image.csv" \
+        # --recursive
         # --dry-run
 
     ## download the CellProfiler single-cell profiles and metadata for snakemake analysis pipeline
     # aws s3 sync --no-sign-request --exclude "*.csv" "$AWS_WORKSPACE_PATH/backend/$batch_id" $SNAKEMAKE_INPUT_PATH/single_cell_profiles/$batch_id
-    aws s3 sync --no-sign-request "$AWS_WORKSPACE_PATH/metadata/platemaps/$batch_id" $SNAKEMAKE_INPUT_PATH/metadata/platemaps/$batch_id
+    # aws s3 sync --no-sign-request "$AWS_WORKSPACE_PATH/metadata/platemaps/$batch_id" $SNAKEMAKE_INPUT_PATH/metadata/platemaps/$batch_id
 done
